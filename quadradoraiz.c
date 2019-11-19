4260a2d410c40c6505d599ae14c4c0421cb6c819
Vitor Emanuel <veslima3@gmail.com>
2019-11-19 10:17:27 -0300

    Update quadradoraiz.c

--- quadradoraiz.c
+++ quadradoraiz.c
@@ -1,6 +1,3 @@
-// 1   Escreva um programa que leia um numero inteiro positivo e apresente o quadrado e a raiz quadrada desse numero
-
-
 #include <stdio.h>
 #include <math.h>
 
@@ -9,13 +6,13 @@
 int main()
 {
     
-    //a tipo de variável afeta a computação no if
+    //o tipo de variável afeta a computação no if
     float numero, raiz, quadrado;
     
     printf("Informe um número inteiro e positivo\n");
     scanf("%f", &numero);
     
-    if (numero < 1 || numero != (int)numero ){
+    if (numero < 0 || numero != (int)numero ){
         
         printf("Reinicie o programa e digite um numero inteiro e positivo\n");
         
