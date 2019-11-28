8ae66384259c2235093ef398917513b7eb2c7754
Vitor Emanuel <veslima3@gmail.com>
2019-11-27 21:57:43 -0300

    Update exercicio16.c

--- exercicio16.c
+++ exercicio16.c
@@ -4,25 +4,25 @@
 int main()
 {
     
-    int cent, dez, uni;
+//     int cent, dez, uni;
     
-    printf("Insira o número da centena.\n");
-    scanf("%d", &cent);
+//     printf("Insira o número da centena.\n");
+//     scanf("%d", &cent);
     
-    printf("Insira o número da dezena.\n");
-    scanf("%d", &dez);
+//     printf("Insira o número da dezena.\n");
+//     scanf("%d", &dez);
     
-    printf("Insira o número da centena.\n");
-    scanf("%d", &uni);
+//     printf("Insira o número da centena.\n");
+//     scanf("%d", &uni);
     
     
-    printf(" O seu número é %d%d%d.\n", cent, dez, uni);
+//     printf(" O seu número é %d%d%d.\n", cent, dez, uni);
     
-   int num [] = {cent, dez, uni};
+//    int num [] = {cent, dez, uni};
    
    
     
-   printf("O número ao contrário é: %d%d%d.\n", num[2], num[1], num[0]);
+//    printf("O número ao contrário é: %d%d%d.\n", num[2], num[1], num[0]);
     
     
     return 0;
