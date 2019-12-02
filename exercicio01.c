7bd68244af21c9058e26ba0d174e9e8a1c4e60f8
Vitor Emanuel <veslima3@gmail.com>
2019-12-01 23:07:37 -0300

    Create exercicio01.c

---
+++ exercicio01.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int base, altura, area, perimetro;
+    
+    printf("Informe o comprimento das bases.\n");
+    scanf("%d", &base);
+    
+    printf("Informe a altura.\n");
+    scanf("%d", &altura);
+    
+    area = base * altura;
+    
+    printf("A area é de %d. \n", area);
+    
+    perimetro = (base * 2) + (altura * 2);
+    
+    printf("O perímetro é de %d.", perimetro);
+    
+    return 0;
+}
