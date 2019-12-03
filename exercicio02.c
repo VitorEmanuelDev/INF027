16f6e1fc65f82379031718bc377cc3d4e8f4b7ff
Vitor Emanuel <veslima3@gmail.com>
2019-12-03 12:55:49 -0300

    Create exercicio02.c

---
+++ exercicio02.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int lado, area, perimetro;
+    
+    printf("Informe o tamanho do lado do quadrado em cm.\n");
+    scanf("%d", &lado);
+    
+    area = pow(lado, 2);
+    
+    perimetro = lado *4;
+    
+    printf("A área é de %d cm quadrados e o perímetro é de %d cm.\n", area, perimetro);
+
+    return 0;
+}
