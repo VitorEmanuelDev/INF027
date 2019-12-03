3b34ad34cf363df8e4a94a13019303d928a9f9cd
Vitor Emanuel <veslima3@gmail.com>
2019-12-03 19:51:56 -0300

    Create exercicio12.c

---
+++ exercicio12.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+float main()
+{
+    float nota1, nota2, nota3, nota4, media1, media2, mediaFinal;
+    
+    printf("Informe a primeira nota do primeiro bimestre.\n");
+    scanf("%f", &nota1);
+    
+    printf("Informe a segunda nota do primeiro bimestre.\n");
+    scanf("%f", &nota2);
+    
+    printf("Informe a primeira nota do segundo bimestre.\n");
+    scanf("%f", &nota3);
+    
+    printf("Informe a segunda nota do segundo bimestre.\n");
+    scanf("%f", &nota4);
+    
+    media1 = (nota1 + nota2)/2;
+    
+    media2 = (nota3 + nota4)/2;
+    
+    mediaFinal = (media1 + media2)/2;
+    
+    printf("A média do primeiro semestre é de %.1f.\n", media1);
+    
+    printf("A média do segundo semestre é de %.1f.\n", media2);
+    
+    printf("A média final é de %.1f.\n", mediaFinal);
+     
+    return 0;
+}
