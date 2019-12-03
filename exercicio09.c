a6a6e25d0ccc80358aba66dc43bc321ed025ef26
Vitor Emanuel <veslima3@gmail.com>
2019-12-03 15:00:16 -0300

    Create exercicio09.c

---
+++ exercicio09.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float raio, pi = 3.14, area, perimetro, volume, altura;
+    
+    printf("Informe o raio da circunferência da lata em metros.\n");
+    scanf("%f", &raio);
+    
+    area = pi * pow(raio, 2);
+    
+    printf("A area da base é de %.2f metros quadrados.\n", area);
+    
+    perimetro = 2 * pi * raio;
+    
+    printf("O perímetro da base é de %.2f metros.\n\n", perimetro);
+    
+    printf("Informe a altura. \n");
+    scanf("%f", &altura);
+
+    volume = area * altura;
+    
+    printf("O volúme da lata é de %.2f metros cubicos", volume);
+
+    return 0;
+}
+
