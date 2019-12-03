21c55965f0413600122cae1ca3c85b87cc0d4f8f
Vitor Emanuel <veslima3@gmail.com>
2019-12-03 14:51:58 -0300

    Create exercicio06.c

---
+++ exercicio06.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+ int main()
+{
+    int numero1, numero2, quociente, resto;
+    
+    printf("Informe o numerador.\n");
+    scanf("%d", &numero1);
+    printf("Informe o denominador.\n");
+    scanf("%d", &numero2);
+    
+    quociente = numero1/numero2;
+    
+    resto = numero1%numero2;
+    
+    printf("O quociente é de %d e o resto é de %d .", quociente, resto);
+
+    return 0;
+}
+
