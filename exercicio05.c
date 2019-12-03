5320884ff99c2a4f651b4cef2c5c1f9ef8bd7348
Vitor Emanuel <veslima3@gmail.com>
2019-12-03 13:52:19 -0300

    Create exercicio05.c

---
+++ exercicio05.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero, sucessor;
+    
+    printf("Informe um número inteiro.\n");
+    scanf("%d", &numero);
+
+    sucessor = numero + 1;
+    
+    printf("O sucessor é %d.\n", sucessor);
+
+    return 0;
+}
