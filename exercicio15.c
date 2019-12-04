97e78357d903708f969aa375394674b6e0a46128
Vitor Emanuel <veslima3@gmail.com>
2019-12-04 12:09:41 -0300

    Update exercicio15.c

--- exercicio15.c
+++ exercicio15.c
@@ -1,3 +1,7 @@
+//Num dia de sol, você deseja medir a altura de um prédio, porém, a trena não é suficientemente longa. Assumindo 
+//que seja possível medir sua sombra e a do prédio no chão, e que você lembre da sua altura, faça um programa para
+//ler os dados necessários e calcular a altura do prédio.
+
 #include <stdio.h>
 
 int main()
@@ -16,3 +20,5 @@ int main()
     
     printf("A altura do prédio é %.2f m.\n", alturaPredio);
     
+    return 0;
+}
