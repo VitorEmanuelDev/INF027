9cde3169196a63677829ff757a459c3155fd6d92
Vitor Emanuel <veslima3@gmail.com>
2019-12-04 11:30:45 -0300

    Update exercicio08.c

--- exercicio08.c
+++ exercicio08.c
@@ -1,3 +1,5 @@
+//Dado que a fórmula para conversão de Fahrenheit para Celsius é C = 5/9 (F – 32), leu um valor de temperatura em Fahrenheit e exibi-lo em Celsius
+
 #include <stdio.h>
 
 int main()
