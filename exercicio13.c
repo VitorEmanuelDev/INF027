a10228ed4db5edb65545c17405a8789c8fc6e7cf
Vitor Emanuel <veslima3@gmail.com>
2019-12-04 11:51:20 -0300

    Update exercicio13.c

--- exercicio13.c
+++ exercicio13.c
@@ -1,3 +1,5 @@
+//Um circuito elétrico é composto de duas resistências R1 e R2 em paralelo, e ambas em sequência de uma resistência R3. Faça um algoritmo para calcular a resistência equivalente desse circuito.
+
 #include <stdio.h>
 
 
