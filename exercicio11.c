895109695964e40e11323acad35a314529b41a16
Vitor Emanuel <veslima3@gmail.com>
2019-12-04 11:32:25 -0300

    Update exercicio11.c

--- exercicio11.c
+++ exercicio11.c
@@ -1,3 +1,5 @@
+//Faça um algoritmo para calcular a nota semestral de um aluno. A nota semestral é obtida pela média aritmética entre a nota de 2 bimestres. Cada nota de bimestre é composta por 2 notas de provas.
+
 #include <stdio.h>
 
 float main()
