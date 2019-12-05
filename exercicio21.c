0fcd6a85b28e39bd695fafc9c7ac32e33abc8528
Vitor Emanuel <veslima3@gmail.com>
2019-12-05 20:04:05 -0300

    Create exercicio21.c

---
+++ exercicio21.c
@@ -0,0 +1,24 @@
+//Escreva um programa que leia um número e exiba se ele é positivo ou negativo
+
+
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+    
+    printf("Informe um número");
+    scanf("%d", &numero);
+    
+    if (numero > 0){
+        
+        printf("O número %d é positivo", numero);
+        
+    } else {
+        
+        printf("O número %d é negativo", numero);
+        
+    }
+
+    return 0;
+}
