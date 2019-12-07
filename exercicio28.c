91fa4429c64af974973397ef68e57e3fe9e7fc26
Vitor Emanuel <veslima3@gmail.com>
2019-12-07 01:21:20 -0300

    Create exercicio28.c

---
+++ exercicio28.c
@@ -0,0 +1,26 @@
+//Escreva um programa que leia uma letra e mostre se ela é vogal ou consoante.
+
+#include <stdio.h>
+
+int main()
+{
+    
+    int i;
+    char letra;
+    char vogal[] = "aeiou"; 
+    
+    printf("informe uma letra.\n");
+    scanf("%c", &letra);
+    
+    if(letra == vogal[i]){
+        
+        printf("A letra '%c' é uma vogal.", letra);
+        
+    } else {
+        
+        printf("A letra '%c' não é uma vogal.", letra);
+        
+    }
+
+    return 0;
+}
