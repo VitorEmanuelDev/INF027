801fb652dd0ed37eb83b7bb0054c2239795378de
Vitor Emanuel <veslima3@gmail.com>
2019-12-10 09:15:24 -0300

    Create exercicio32.c

---
+++ exercicio32.c
@@ -0,0 +1,38 @@
+/*Escreva um programa que leia um caracter e diga se ele é uma vogal, consoante, número
+ou um símbolo (qualquer outro caracter, que não uma letra ou número).*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    char elemento [] = "abcdefghijklmnopqrstuvxz0123456789";
+    
+    printf("Informe um número ou uma letra ou um símbolo");
+    scanf("%c", elemento);
+    
+    if(elemento[i] == 0 || elemento[i] == 4 || elemento[i] == 8 
+    || elemento[i] == 14 || elemento[i] == 20) {
+        
+        printf("O caracter escolhido é uma vogal.");
+        
+    } else if (elemento[i] != 0 || elemento[i] != 4 || elemento[i] != 8 
+    || elemento[i] != 14 || elemento[i] != 20 && elemento[i] <= 23){
+        
+        printf("O caracter escolhido é uma consoante.");
+        
+    } else if( elemento[i] > 23){
+        
+        printf("O caracter escolhido é um número.");
+        
+    } else {
+        
+        printf("O caracter escolhido é um símbolo.");
+        
+    }
+
+    return 0;
+}
+
+
+
