6bd6362eeb97dccfc1f073bda0aa2ce4868ebef1
Vitor Emanuel <veslima3@gmail.com>
2019-12-18 15:14:58 -0300

    Update avaliacao1Q2.c

--- avaliacao1Q2.c
+++ avaliacao1Q2.c
@@ -24,116 +24,7 @@ aplicado no cálculo do prêmio. Escreva um programa em C, que leia o valor apos
 escolhido pelo apostador, e o número sorteado, calcule e imprima qual o prêmio que o apostador deve
 receber.*/
 
-#include <stdbool.h> 
-#include <stdlib.h>
-#include <stdio.h>
 
-int main()
-{
-    int M = rand() % 100000; // Retorna um inteiro pseudo randômico entre 0 e RAND_MAX.
-    bool milharCerto = true, centenaCerta = true, dezenaCerta = true, unidadeCerta = true;
-    int N, V, milharNG, milharNP, centenaN, dezenaN, unidadeN,
-    milharMG, milharMP, centenaM, dezenaM, unidadeM,
-    premio;
-    
-    printf("Informe o valor da aposta.\n");
-    scanf("%d", &V);
-    
-    printf("Informe o número no qual deseja apostar.\n");
-    scanf("%d", &N);
-    
-    if((V < 0 || V > 99999) || (N < 0 || N > 99999)){
-        
-        printf("Valor inválido.");
-        
-    } else {
-        
-        milharNG = N/10000;
-        
-        milharNP = N/1000;
-        
-        centenaN = (N/100)%10;
-        
-        dezenaN = (N%100)/10;
-        
-        unidadeN = N%10;
-        
-        
-        milharMG = M/10000;
-        
-        milharMP = M/1000;
-        
-        centenaM = (M/100)%10;
-        
-        dezenaM = (M%100)/10;
-        
-        unidadeM = M%10;
-        
-        if((milharMP == milharNP || milharNP == milharMG ||
-        milharNG == milharMP || milharMG == milharNG)){
-            
-            return milharCerto;
-            
-        }
-        
-        if(centenaM == centenaN){
-            
-            return centenaCerta;
-            
-        }
-        
-        if(dezenaM == dezenaN){
-            
-            return dezenaCerta;
-            
-        }
-        
-        if(unidadeM == unidadeN){
-            
-            return unidadeCerta;
-            
-        }
-        
-        
-        if(milharCerto && centenaCerta && dezenaCerta && unidadeCerta){
-            
-            premio = V * 3000;
-            
-            printf("Parabéns! Você acertou os 4 últimos números e ganhou R$ %d", premio);
-            
-        } else if (milharCerto == false){
-            
-            premio = V * 500;
-            
-            printf("Parabéns! Você acertou os 3 últimos números e ganhou R$ %d", premio);
-            
-        } else if (milharCerto == false && centenaCerta == false){
-            
-            premio = V * 50;
-            
-            printf("Parabéns! Você acertou os 2 últimos números e ganhou R$ %d", premio);
-            
-        } else if (milharCerto == false && centenaCerta == false && dezenaCerta == false){
-            
-            premio = V * 16;
-            
-            printf("Parabéns! Você acertou o último digito e ganhou R$ %d", premio);
-           // rever regra para implementar: se M e N têm os dois últimos dígitos no mesmo grupo, correspondendo ao mesmo animal, o
-          // apostador recebe V × 16 (por exemplo, N = 82197 and M = 337600);
-        } else {
-            
-            printf("Infelizmente você não conseguiu acertar.");
-            
-        }
-        
-    }
-    
-    
-    
-
-    return 0;
-}
-/*
 
 #include <stdio.h>
 
@@ -159,21 +50,37 @@ main() {
 	dezenaSorteada = sorteado % 100;
 	dezenaApostada = apostado % 100;
 	
-	if (dezenaSorteada == 00)
+	if (dezenaSorteada == 00){
+		
 	  dezenaSorteada = 100;
-	if (dezenaApostada == 00)
+		
+	}
+	
+	if (dezenaApostada == 00){
+		
 	  dezenaApostada = 100;
+	
+	}
+	
 	grupoSorteado = ((dezenaSorteada - 1) / 4) + 1; 
 	grupoApostado = ((dezenaApostada - 1) / 4) + 1;
 	
-	if(apostado % 10000 == sorteado % 10000)
-		multiplicador = 3000;  
-	else if(apostado % 1000 == sorteado % 1000)
+	if(apostado % 10000 == sorteado % 10000){
+		
+		multiplicador = 3000;
+		
+	} else if(apostado % 1000 == sorteado % 1000){
+		
 		multiplicador = 500;  
-	else if(apostado % 100 == sorteado % 100)
-		multiplicador = 50;  
-	else if(grupoSorteado == grupoApostado)
+		
+	} else if(apostado % 100 == sorteado % 100){
+		
+		multiplicador = 50;
+		
+	} else if(grupoSorteado == grupoApostado){
+		
 		multiplicador = 16;  	
+	}
 	
 	premio = valorAposta * multiplicador;
 	
@@ -181,8 +88,5 @@ main() {
 	printf("Grupo Apostado: %d\n", grupoApostado);	
 	printf("Premio: %.2f", premio);
 	
-	
 }
 
-
-*/
