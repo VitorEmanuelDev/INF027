971eaff32ab7309ee1a80eb8a28bcac68d16b23d
Vitor Emanuel <veslima3@gmail.com>
2019-12-29 02:28:11 -0300

    Create EX17idadeDetalhada.c

---
+++ livrosC/EX17idadeDetalhada.c
@@ -0,0 +1,27 @@
+/*Fazer um algoritmo que leia a idade
+de uma pessoa expressa em dias e mostre-a
+expressa em anos, meses e dias.*/
+
+#include <stdio.h>
+
+int main()
+{
+    
+    int idadeAnos, restoIdadeAnos, idadeMes, idadeDias, ano = 365, mes = 30;
+    
+    printf("Qual a idade da pessoa em dias?\n");
+    scanf("%d", &idadeDias);
+    
+    idadeAnos = idadeDias/ano;
+    
+    restoIdadeAnos = idadeDias%ano;
+    
+    idadeMes = restoIdadeAnos/mes;
+    
+    idadeDias = idadeMes%mes;
+    
+    printf("A pessoa tem %d anos, %d meses e %d dias.", idadeAnos, idadeMes, idadeDias);
+
+    return 0;
+}
+
