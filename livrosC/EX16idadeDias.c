2b57114e0d28cc09c01ec55768b151e4576b9cd8
Vitor Emanuel <veslima3@gmail.com>
2019-12-29 01:20:20 -0300

    Create EX16idadeDias.c

---
+++ livrosC/EX16idadeDias.c
@@ -0,0 +1,24 @@
+/*Fazer um algoritmo que leia a idade
+de uma pessoa expressa em anos, meses
+e dias e mostre-a apenas em dias.*/
+
+#include <stdio.h>
+
+int main()
+{
+    
+    float idade, meses, dias, idadeDias, diasAno = 365, diasMes = 30.4375;
+    
+    printf("Quantos anos a pessoa tem?\n");
+    scanf("%f", &idade);
+    printf("%.0f anos e quantos meses?\n", idade);
+    scanf("%f", &meses);
+    printf("%.0f anos, %.0f meses e quantos dias?\n", idade, meses);
+    scanf("%f", &dias);
+    
+    idadeDias = (idade * diasAno) + (meses * diasMes) + dias; 
+    
+    printf("A idade informada em dias é de: %.0f", idadeDias);
+
+    return 0;
+}
