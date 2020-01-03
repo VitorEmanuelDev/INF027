a0ea6305db4647ad9d11fad91c629a0fcf968faf
Vitor Emanuel <veslima3@gmail.com>
2020-01-02 22:26:43 -0300

    Create EX26numInvertido.c

---
+++ livrosC/EX26numInvertido.c
@@ -0,0 +1,24 @@
+/*Escrever um algoritmo que lê um número de 3 dígitos
+e o inverta, escrevendo o número invertido.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int num, centena, dezena, unidade, resto, numNovo;
+    
+    printf("Informe um número de três digitos.\n");
+    scanf("%d", &num);
+    
+    centena = num/100; // 9 = 987/100;
+    resto = num%100; // 87 = 987%100;
+    dezena = resto/10; // 8 = 87/10;
+    unidade = resto%10; // 7 = 87%10;
+    
+    numNovo = (unidade * 100) + (dezena * 10) + centena;
+    
+    printf("O número invertido é %d", numNovo);
+
+    return 0;
+}
+
