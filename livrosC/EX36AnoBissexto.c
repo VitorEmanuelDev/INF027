ea3d50c6da22e9cf2f3e6684cf26721517c4227c
Vitor Emanuel <veslima3@gmail.com>
2020-01-05 20:50:39 -0300

    Update and rename EX36mesAno.c to EX36AnoBissexto.c

---
+++ livrosC/EX36AnoBissexto.c
@@ -0,0 +1,24 @@
+/*Desenvolver o ano e dizer se ele é ou não bissexto*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{  
+    int ano;
+  
+    
+    printf("Informe um ano:\n");
+    scanf("%d", &ano);
+    
+    if(ano%4 == 0){
+        
+        printf("%d tem 366 dias.\n", ano);
+          
+    } else if (ano%4 != 0){
+        
+        printf("%d tem 365 dias.\n", ano);
+    
+    }
+ return 0;   
+}
