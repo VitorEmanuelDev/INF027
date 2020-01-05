ae835f4ddb5d2c267cf93b79140cc34be3234609
Vitor Emanuel <veslima3@gmail.com>
2020-01-05 13:12:06 -0300

    Update EX34equacaoRaiz.c

--- livrosC/EX34equacaoRaiz.c
+++ livrosC/EX34equacaoRaiz.c
@@ -4,13 +4,33 @@ suas raízes. O programa deve mostrar, quando possível,
 o valor das raízes calculadas e a classificação das raízes.*/
 
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    float a, b, c, x1, x2;
+    float a, b, c, x1, x2, delta;
     
     printf("Dada uma equação do tipo ax² + bx + c, informe os coeficientes.\n");
-    scanf();
-
+    scanf("%f%f%f", &a, &b, &c);
+    
+    delta = pow(b,2) - (4*a*c);
+    
+    x1 = (-b + sqrt(delta))/2*a;
+    x2 = (-b - sqrt(delta))/2*a;
+    
+    if(delta == 0){
+        
+        printf("A solução da equação é %.2f.", x1);
+        
+    } else if(delta < 0){
+        
+        printf("Sem solução para números reais.");
+        
+    } else if (delta > 0){
+        
+        printf("As raízes da equação informada são: %.2f e %.2f", x1, x2);
+        
+    }
+    
     return 0;
 }
