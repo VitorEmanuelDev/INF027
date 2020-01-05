ce155bd0a7131beff0f736ddd40f9a2b267b5f49
Vitor Emanuel <veslima3@gmail.com>
2020-01-05 03:45:34 -0300

    Create EX34equacaoRaiz.c

---
+++ livrosC/EX34equacaoRaiz.c
@@ -0,0 +1,16 @@
+/*Desenvolver um algoritmo que leia os coeficientes
+(a , b e c) de uma equação do segundo grau e calcule
+suas raízes. O programa deve mostrar, quando possível,
+o valor das raízes calculadas e a classificação das raízes.*/
+
+#include <stdio.h>
+
+int main()
+{
+    float a, b, c, x1, x2;
+    
+    printf("Dada uma equação do tipo ax² + bx + c, informe os coeficientes.\n");
+    scanf();
+
+    return 0;
+}
