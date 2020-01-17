b2fc0eb1ed56277d5506f09cccfc65ffb9262ef8
Vitor Emanuel <veslima3@gmail.com>
2020-01-17 18:07:11 -0300

    To be continued
    Só funciona com os blocos repetidos.

---
+++ livrosC/EX66operacoes.c
@@ -0,0 +1,69 @@
+/*Criar um algoritmo que leia dois números inteiros,
+e que solicite ao usuário qual a operação deseja realizar
+entre esses números. Caso o usuário digitar o caractere “x”
+será realizada uma multiplicação, caso seja digitado o caractere
+“ / ”' será realizada uma divisão, caso seja digitado o caractere
+‘’+’’ será realizado uma adição, e caso seja digitado o caractere
+‘’ – ‘’ será realizada uma subtração.*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    char operacao;
+    int num1, num2, resultado;
+    
+    printf("Informe dois números inteiros:\n");
+    scanf("%d%d", &num1, &num2);
+    
+    printf("Qual operação deseja realizar?\n");
+    printf("Aperte '+' para adicionar\nAperte '-' para subtrair\nAperte 'x' para multiplicar\nAperte '/' para dividir.");
+    scanf("%c", &operacao);    
+    
+    if(operacao == '+'){
+        
+        resultado = num1 + num2;
+        
+    } else if(operacao == '-'){
+        
+        resultado = num1 - num2;
+        
+    }else if(operacao == '/'){
+        
+        resultado = num1 / num2;
+        
+    }else if(operacao == 'x'){
+        
+        resultado = num1 * num2;
+        
+    }
+    
+    printf("%d %c %d = %d", num1, operacao, num2, resultado);
+    
+    printf("Qual operação deseja realizar?\n");
+    printf("Aperte '+' para adicionar\nAperte '-' para subtrair\nAperte 'x' para multiplicar\nAperte '/' para dividir.\n");
+    scanf("%c", &operacao);    
+    
+    if(operacao == '+'){
+        
+        resultado = num1 + num2;
+        
+    } else if(operacao == '-'){
+        
+        resultado = num1 - num2;
+        
+    }else if(operacao == '/'){
+        
+        resultado = num1 / num2;
+        
+    }else if(operacao == 'x'){
+        
+        resultado = num1 * num2;
+        
+    }
+    
+    printf("%d %c %d = %d", num1, operacao, num2, resultado);
+    return 0;
+}
+
