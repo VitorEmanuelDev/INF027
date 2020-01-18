482f3ed2e3103487e9bce4211ea4a110515dc489
Vitor Emanuel <veslima3@gmail.com>
2020-01-18 08:21:09 -0300

    Create EX67valorProduto.c

---
+++ livrosC/EX67valorProduto.c
@@ -0,0 +1,22 @@
+/*Elabore um algoritmo que calcule o que deve
+ser pago por um produto, considerando o preço
+normal de etiqueta e a escolha da condição de
+pagamento. Utilize os códigos da tabela a seguir
+para ler qual a condição de pagamento escolhida
+e efetuar o cálculo adequado.
+
+1 - À vista em dinheiro, recebe 15% de desconto 
+2 - À vista no cartão de crédito, recebe 10% de desconto 
+3 - Em duas vezes, preço normal de etiqueta sem juros 
+4 - Em três vezes, preço normal de etiqueta mais juros de 10%
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}
