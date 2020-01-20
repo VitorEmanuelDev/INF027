9fefbebca1925ffe0b2253ccd15adcaa641791cc
Vitor Emanuel <veslima3@gmail.com>
2020-01-20 16:33:54 -0300

    Create exercicio41.c

---
+++ exercicio41.c
@@ -0,0 +1,20 @@
+/*Escreva um programa qe imprima todos os
+números inteiros do intervalo fechado de 1 a 100.
+
+*/
+#include <stdio.h>
+
+int main(){
+
+    int i = 1;
+
+    while(i <= 100){
+
+        printf("%d\n",i);
+
+        i += 1;
+
+    }
+    
+    return 0;
+}
