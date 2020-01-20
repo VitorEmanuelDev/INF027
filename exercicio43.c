fb4cc685b1a2ba188ff10075ddeb6ab6fc6590df
Vitor Emanuel <veslima3@gmail.com>
2020-01-20 16:46:32 -0300

    Create exercicio43.c

---
+++ exercicio43.c
@@ -0,0 +1,18 @@
+/*Escreva um programa que imprima todos
+os números pares do intervalo fechado de 1 a 100.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 2;
+    
+    while(i <= 100){
+        
+        printf("%d\n", i);
+        i += 2;
+        
+    }
+
+    return 0;
+}
