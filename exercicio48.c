c9752b3c065d7e9a580080471adc2d2624da4bd6
Vitor Emanuel <veslima3@gmail.com>
2020-01-23 15:44:44 -0300

    Create exercicio48.c

---
+++ exercicio48.c
@@ -0,0 +1,44 @@
+/*Escreva um programa que determine se um dado
+número N (digitado pelo usuário) é primo ou não.*/
+
+#include <stdio.h>
+
+int main() {
+    
+    int num, i, contagem = 0;
+    
+    printf("Informe um número inteiro (num > 0):\n");
+    scanf("%d", &num);
+    
+    if (num < 0){
+        
+        printf("Número positivo, pow!");
+        
+        
+    }else{
+    
+    for (i = 2; i <= num/2; i++) {
+        
+        //não é número primo se...
+        if (num%i == 0) {
+            
+            contagem++;
+            break;
+            //faça contagem + 1 e pare o programa
+        }
+        
+    }
+    
+    if(contagem == 0 && num != 1 ){
+    //se não houve contagem e se o número informado não for igual a 1  
+   	printf("%d é um número primo.", num);
+        
+    }else{
+        
+   	printf("%d não é um número primo", num);
+   	
+   }
+}
+  return 0;
+  
+}
