10139e27d744eb846081cf72fb4325e538eb0a95
Vitor Emanuel <veslima3@gmail.com>
2020-01-22 23:23:14 -0300

    Create exercicio47.c

---
+++ exercicio47.c
@@ -0,0 +1,35 @@
+/*Escreva um programa que calcule o resto da divisão
+de A por B (número inteiros e positivos), ou seja, A % B,
+através de subtrações sucessivas. Esses dois valores são passados pelo
+usuário através do teclado.*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int A, B, i, a;
+    
+    printf("Informe o dividendo 'A' e o divisor 'B'(sendo A > B):\n");
+    scanf("%d%d", &A, &B);
+
+    if(A < 0 || B < 0 || A < B){
+        
+        printf("Quociente inválido. Informe números positivos (sendo A > B).\n");
+        
+    }
+
+    i = 0;
+    a = A;
+
+    while (a >= B) {
+        
+        a = a - B;
+        
+        i++;
+    }
+
+    printf("%d dividido por %d tem resto igual a %d", A, B, a);
+
+    return 0;
+}
