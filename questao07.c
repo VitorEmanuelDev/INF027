2acfafd08ea69373aff80dc2cc2662da4f98059b
Vitor Emanuel <veslima3@gmail.com>
2020-01-30 01:17:54 -0300

    Create questao07.c

---
+++ questao07.c
@@ -0,0 +1,33 @@
+/*Questão 07 Prof Luiz Machado
+
+obs: enunciado com licença "poética"
+
+Depois de uma longa jornada de sefudência, Mario e Luigi chegaram
+à fortaleza onde encontra-se, a sempre perdida, princesa Peach, mas
+antes de conseguirem finalmente resgatar a princesa, eles terão mais
+um desafio pela frente,um sacana feio como a d*sgraça chamado Bowser.
+
+Os nossos jovens estarão equipados com uma das "flores" ;) que lhes concedem
+"poderes especiais", e os respectivos danos estão definidos na tabela a seguir:
+
+- Capim: Mário 32, Luigi 28;
+- Erva doce: 50, 45;
+- Girassol: 73, 70;
+
+O nosso herói lazarento será capaz de desferir (uia!) 4 porradas consecutivos no
+Bowser antes que ele se pique. Caso o dano causado pelo nosso herói seja maior, ou igual, à
+vida do coisa ruim, ele consegue fugir com a princesa para sempre...
+
+
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    char player[25]
+    
+    printf("");
+
+    return 0;
+}
