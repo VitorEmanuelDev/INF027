c74ff6f5e72bec5204e5eec69654eab6ac6e26f9
Vitor Emanuel <veslima3@gmail.com>
2020-02-02 01:36:56 -0300

    Create exercicio50.c

---
+++ exercicio50.c
@@ -0,0 +1,57 @@
+/*Escreva um programa que leia um valor e imprima todas as possíveis combinações em que o
+lançamento de um par de dados tenha como resultado da soma dos valores dos dados o
+número lido. Por exemplo, se a entrada for o número 7, o programa deve imprimir as
+seguintes combinações:
+
+- 1 6
+- 2 5
+- 3 4
+- 4 3
+- 5 2
+- 6 1
+
+*/
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int num, contador;
+    
+    printf("Informe um número.\n");
+    scanf("%d", &num);
+    
+    if (num < 0){
+        
+        printf("Valor inválido");
+        
+    }
+    
+    /*num = sqrt((num + 1) * 2);  
+    num = pow(num, 2);
+    
+    for (contador = 1; contador < 7; contador++){
+        
+        printf("%d\n", num);
+        num = num + 9;
+        
+    }*/
+    
+    
+    /*
+    for (contador = 1; contador < num; contador++){
+        
+        printf("%d\n", contador);
+        
+    }
+    
+    for (contador = num; contador > 1; contador--){
+        
+        num = num - 1;
+        printf("%d\n", num);
+        
+    }*/
+    
+    
+    return 0;
+}
