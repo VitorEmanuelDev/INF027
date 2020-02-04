b8b5da2f580e0d25eed79f2410c240621cbb5ca3
Vitor Emanuel <veslima3@gmail.com>
2020-02-04 19:26:45 -0300

    Update exercicio52.c

--- exercicio52.c
+++ exercicio52.c
@@ -5,9 +5,33 @@ anteriores, ou seja:
 1. A i = A i-1 + A i-2 , para i ímpar
 2. A i = A i-1 - A i-2 , para i par
 
-Criar um algoritmo em PORTUGOL que imprima os N primeiros termos da série de
+Criar um algoritmo em C que imprima os N primeiros termos da série de
 FETUCCINE, sabendo-se que para existir esta série serão necessários pelo menos três termos.*/
+
 #include<stdio.h>
 
+int main(){
+int termo1, termo2, i, termo3;
 
+    printf("Informe o primeiro termo:\n ");
+    scanf("%d", &termo1);
+    
+    printf("Informe o segundo termo:\n ");
+    scanf("%d", &termo2);
+
+    for(i = 3; i <= 10; i++){
+
+        if(i%2 == 0 || i%2 == 0){
+    
+            termo3 = (i - 1) - (i - 2);
+            printf("%d ", termo3);
+
+        } else {
+            
+            termo3 = (i - 1) + (i - 2);
+            printf("%d ", termo3);
+            
+                
+        }
+    }
 }
