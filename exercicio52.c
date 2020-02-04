1d99c5a675eb5e744bdffb6e23dedd72975abcb1
Vitor Emanuel <veslima3@gmail.com>
2020-02-04 07:37:09 -0300

    Update exercicio52.c

--- exercicio52.c
+++ exercicio52.c
@@ -9,29 +9,5 @@ Criar um algoritmo em PORTUGOL que imprima os N primeiros termos da série de
 FETUCCINE, sabendo-se que para existir esta série serão necessários pelo menos três termos.*/
 #include<stdio.h>
 
-int main(){
-int a, b, i, termo;
 
-    printf("Digite um Numero: ");
-    scanf("%d", &a);
-    
-    printf("Digite outro Numero: ");
-    scanf("%d", &b);
-
-    for(i=3; i <= 10; i++){
-
-        if(i%2 == 0){
-    
-            termo = (i - 1) - (i - 2);
-
-            printf("%d ", termo);
-
-        } else {
-            
-            termo=(i - 1) + (i - 2);
-            printf("%d ", termo);
-            
-                
-        }
-    }
 }
