7651cc48ab10edacd942447c1e27800e1e959fd3
Vitor Emanuel <veslima3@gmail.com>
2020-02-06 14:23:33 -0300

    Update exercicio54.c

--- exercicio54.c
+++ exercicio54.c
@@ -6,31 +6,31 @@ número é triangular quando é resultado do produto de três números consecuti
 #include <stdio.h>
 
 int main(){
-int numero, contador = 1, verificador, resto;
+int numero, contador = 1, verificador, aux = 0;
 
 printf("Informe um número inteiro:\n");
 scanf("%d", &numero);
 
-//numero = n * (n + 1) * (n + 2);
-
 for (contador = 1; contador <= numero; contador++){
 
     verificador = contador * (contador + 1) * (contador + 2);
-    resto = numero%contador;
     
-    if(resto == 0 && verificador == numero){
+    if(verificador == numero){
         
         printf("%dx%dx%d = %d", contador, contador + 1, contador + 2, numero);
         
         printf("\n%d é um número triangular.\n", numero);
         break;
     
-        } /*else if (numero != verificador && resto == 0 && contador > 1){
+        }
+        
+    }
+    
+    if (numero != verificador){
             
         printf("%d não é um número triangular.\n", numero);
-        break;
             
-        }*/
+        }
     
         return 0;
 }
