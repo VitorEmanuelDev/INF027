941334b59fe16025ee51adf80877b706541bad8f
Vitor Emanuel <veslima3@gmail.com>
2020-02-13 01:23:51 -0300

    Create exercicio58.c

---
+++ exercicio58.c
@@ -0,0 +1,30 @@
+/*O número e (número de Euler) pode ser representado e calculado por meio da utilização da
+série de Taylor para e quando x=1, como a soma da seguinte série infinita:
+
+e = 1/1! + 1/2! + 1/3! + ... 1/n!
+
+Escreva um programa, que leia o número de termos da série (n) e imprima como saída, o
+cálculo do número de Euler para cada um dos n primeiros elementos da série.
+        */
+#include<stdio.h>
+
+int main ()
+{
+    
+int numero,contador;
+float euler = 1, fator = 1;
+
+printf("Informe um número:\n");
+scanf("%d", &numero);
+
+for( contador = 1; contador <= numero; contador++){
+    
+    fator *= contador;
+    euler += 1/fator;
+    
+}
+
+printf("Euler: %f", euler);
+
+return 0;
+}
