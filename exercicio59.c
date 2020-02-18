77d5c28ae1d6f72f579d3bdfa007f4cc12fa1a09
Vitor Emanuel <veslima3@gmail.com>
2020-02-18 00:28:17 -0300

    Update exercicio59.c

--- exercicio59.c
+++ exercicio59.c
@@ -4,33 +4,43 @@ um programa mostrando que a afirmação é verdadeira para todo número par entr
 O programa deve imprimir o número par e os dois primos que*/
 
 #include <stdio.h>
-#define MIN 0
-#define MAX 100
+#define MIN 500
+#define MAX 1000
 
 int main() {
     
-    int contador1, contador2, contador3, numero1, numero2, primo, divisoes = 0;
-        
-        
-        for (contador1 = MIN; contador1 < MAX; contador1 += 2) {
+    int numero, contador, contadorPrimo, primo;
     
-            
+    
+    for(contador = MIN; contador <= MAX; contador++){
+        
+        if(contador%2 == 0){
+                
+                printf("%d \n", contador); 
                 
         }
         
-        for (contador2 = 1; contador2 < MAX/2; contador2++){
-            
-                numero1 = contador1 - contador2;
-                numero2 = contador1 - numero1;
-                //printf("%d + %d = %d\n", numero1, numero2, contador1);
-                
-                //verificar se numero1 é primo e se numero2 é primo.
-                //se ambos forem primos, mostrar na tela.
-                //depois de resolver isso, colocar este loop dentro do outro
-            }
-            
-
+        /* primo = 0;
         
+        for(contadorPrimo = 1; contadorPrimo <= MAX; contadorPrimo++){
+            
+            
+            
+            if(contador%contadorPrimo == 0){
+                
+                primo++;
+            
+            }
+        }
+        
+        if(primo == 2){
+           
+           printf("%d \n" ,contador); 
+            
+        }*/
+    
+    }
+
     
     return 0;
 }
