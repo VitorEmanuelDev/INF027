eb9f9e63a824652ff953e4db4587f7a6b5d6591c
Vitor Emanuel <veslima3@gmail.com>
2020-02-19 01:17:44 -0300

    Update exercicio59.c

--- exercicio59.c
+++ exercicio59.c
@@ -19,41 +19,34 @@ int main()
         for(int contador1 = 1; contador1 <= MAX/2; contador1++){
             
             //printf("%d\n", contador1);
-            for(int contador2 = MIN; contador2 >= 1; contador2--){
+            for(int contador2 = 1; contador2 <= MAX/2; contador2++){
                
                 //printf("%d\n", contador2);
                
-                /*
-                for(int contador3 = 2; contador3 <= contador1; contador3++){
+                for(int contador3 = 2; contador3 <= MAX/2; contador3++){
                     
-                    //printf("%d\n", contador3);
                     if (contador1%contador3 == 0) {
                         verificador1++;
                     
                     }
                     
-                }
-                
-                for (int contador4 = 2; contador4 <= contador2; contador4++){
-                    
-                    //printf("%d\n", contador4);
-                    if (contador2%contador4 == 0) {
+                    if (contador2%contador3 == 0) {
                         verificador2++;
                     
                     }
                     
                 }
                 
-                if (verificador1 == 0 && verificador2 == 0){
-        
-                   printf("%d + %d = %d", contador2, contador1, contador);
-                   
-                }*/
                 
+                if (verificador1 == 0 && verificador2 == 0) {
+                    
+                
+                }
             }
         }
     }    
 }
+
 
 
 
