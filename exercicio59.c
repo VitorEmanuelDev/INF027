84b7d337f8bc0234c8c1a5c9ef3feea60ff6dd5c
Vitor Emanuel <veslima3@gmail.com>
2020-02-22 23:46:35 -0300

    Update exercicio59.c

--- exercicio59.c
+++ exercicio59.c
@@ -1,19 +1,17 @@
 /*A famosa conjectura de Goldbach diz que todo inteiro par maior que 2 é soma de dois
 números primos. Testes foram feitos, mas ainda não se achou um contra-exemplo. Escreva
 um programa mostrando que a afirmação é verdadeira para todo número par entre 500 e 1000.
-O programa deve imprimir o número par e os dois primos que somados resultam num número par*/
+O programa deve imprimir o número par e os dois primos que somados resultam nesse número par*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
     
-    int primo1, primo2, numero, somaPar, flag1 = 0, flag2 = 0;
-
+    int primo1, primo2, numero, somaPar;
     
         
     for(numero = 2; numero <= 1000; numero++){
-        //printf("\n%d", numPar);
           
         for(primo1 = 2; primo1 <= numero; primo1++){
               
@@ -25,7 +23,6 @@ int main() {
                 
         }
            
-            
         for(primo2 = 2; primo2 <= numero; primo2++){
               
             if(numero%primo2 == 0){
@@ -40,7 +37,7 @@ int main() {
                         
             somaPar = primo1 + primo2;
                         
-            if(somaPar%2 == 0 && somaPar >= 500 && somaPar <= 1000){
+            if(somaPar%2 == 0 && somaPar > 500 && somaPar < 1000){
                             
                 printf("\n%d + %d = %d", primo1, primo2, somaPar);
                             
@@ -50,6 +47,5 @@ int main() {
            
        }
      
-    
 }
 
