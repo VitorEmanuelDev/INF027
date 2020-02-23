9d991d55ec98d5c4bf8975a42354904258aecfa2
Vitor Emanuel <veslima3@gmail.com>
2020-02-22 22:34:13 -0300

    não está pronto

--- exercicio59.c
+++ exercicio59.c
@@ -1,52 +1,58 @@
-/*A famosa conjectura de Goldbach diz que todo inteiro par maior que 2
-é soma de dois números primos. Testes foram feitos, mas ainda não se achou
-um contra-exemplo. Escreva um programa mostrando que a afirmação é verdadeira
-para todo número par entre 500 e 1000. O programa deve imprimir o número par e
-os dois primos que somados dão o número par.*/
+/*A famosa conjectura de Goldbach diz que todo inteiro par maior que 2 é soma de dois
+números primos. Testes foram feitos, mas ainda não se achou um contra-exemplo. Escreva
+um programa mostrando que a afirmação é verdadeira para todo número par entre 500 e 1000.
+O programa deve imprimir o número par e os dois primos que*/
 
 #include <stdio.h>
-#define MIN 500
-#define MAX 1000
+#include <stdlib.h>
 
-
-int main()
-{
-    int verificador1 = 0, verificador2 = 0;
-
-   for(int contador = MIN; contador <= MAX; contador +=2){
-       
-        //printf("%d\n", contador);
-        for(int contador1 = 1; contador1 <= MAX/2; contador1++){
+int main() {
+    
+    int numPar, primo1, primo2, numero, somaPar;
+    int flag1 = 0, flag2 = 0;
+    
+   // for(numPar = 1; numPar <= 10; numPar += 2){
+        
+        for(numero = 500; numero <= 1000; numero++){
+        //printf("\n%d", numPar);
+          
+          for(primo1 = 2; primo1 <= numero/2; primo1++){
+           
+            //printf("\n%d", primo1);
+            for(primo2 = 2; primo2 <= numero/2; primo2++){
             
-            //printf("%d\n", contador1);
-            for(int contador2 = 1; contador2 <= MAX/2; contador2++){
-               
-                //printf("%d\n", contador2);
-               
-                for(int contador3 = 2; contador3 <= MAX/2; contador3++){
+                //printf("\n%d", primo2);
+                if(numero%primo1 == 0){
                     
-                    if (contador1%contador3 == 0) {
-                        verificador1++;
+                        flag1++;
                     
                     }
                     
-                    if (contador2%contador3 == 0) {
-                        verificador2++;
-                    
+                if(numero%primo2 == 0){
+                        
+                        flag2++;
+                        
+                }
+            
+                    if(flag1 != 0 && flag2 != 0){
+                        
+                        somaPar = primo1 + primo2;
+                        
+                        if(primo1%2 != 0 && primo2%2 != 0 && somaPar%2 == 0){
+                            
+                            printf("\n%d + %d = %d", primo1, primo2, somaPar);
+                            
+                        }
+                        
                     }
                     
                 }
-                
-                
-                if (verificador1 == 0 && verificador2 == 0) {
-                    
-                
-                }
-            }
-        }
-    }    
+            
+            }  
+           
+       }
+       
+    //}
+    
 }
-
-
-
 
