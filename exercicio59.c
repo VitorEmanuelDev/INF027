ca5b76ff5bbb6683a04093f6c54f83e0e0b6c448
Vitor Emanuel <veslima3@gmail.com>
2020-02-22 23:31:48 -0300

    em andamento

--- exercicio59.c
+++ exercicio59.c
@@ -1,58 +1,55 @@
 /*A famosa conjectura de Goldbach diz que todo inteiro par maior que 2 é soma de dois
 números primos. Testes foram feitos, mas ainda não se achou um contra-exemplo. Escreva
 um programa mostrando que a afirmação é verdadeira para todo número par entre 500 e 1000.
-O programa deve imprimir o número par e os dois primos que*/
+O programa deve imprimir o número par e os dois primos que somados resultam num número par*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
     
-    int numPar, primo1, primo2, numero, somaPar;
-    int flag1 = 0, flag2 = 0;
+    int primo1, primo2, numero, somaPar, flag1 = 0, flag2 = 0;
+
     
-   // for(numPar = 1; numPar <= 10; numPar += 2){
         
-        for(numero = 500; numero <= 1000; numero++){
+    for(numero = 2; numero <= 1000; numero++){
         //printf("\n%d", numPar);
           
-          for(primo1 = 2; primo1 <= numero/2; primo1++){
+        for(primo1 = 2; primo1 <= numero; primo1++){
+              
+            if(numero%primo1 == 0){
+                        
+                break;
+                        
+            }
+                
+        }
            
-            //printf("\n%d", primo1);
-            for(primo2 = 2; primo2 <= numero/2; primo2++){
             
-                //printf("\n%d", primo2);
-                if(numero%primo1 == 0){
-                    
-                        flag1++;
-                    
-                    }
-                    
-                if(numero%primo2 == 0){
+        for(primo2 = 2; primo2 <= numero; primo2++){
+              
+            if(numero%primo2 == 0){
                         
-                        flag2++;
+                break;
                         
-                }
+            }
+                
+        }      
             
-                    if(flag1 != 0 && flag2 != 0){
+        if(numero == primo1 && numero == primo2){
                         
-                        somaPar = primo1 + primo2;
+            somaPar = primo1 + primo2;
                         
-                        if(primo1%2 != 0 && primo2%2 != 0 && somaPar%2 == 0){
+            if(somaPar%2 == 0 && somaPar >= 500 && somaPar <= 1000){
                             
-                            printf("\n%d + %d = %d", primo1, primo2, somaPar);
+                printf("\n%d + %d = %d", primo1, primo2, somaPar);
                             
-                        }
-                        
-                    }
-                    
                 }
-            
-            }  
+                        
+            }
            
        }
-       
-    //}
+     
     
 }
 
