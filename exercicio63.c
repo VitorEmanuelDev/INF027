aa3e6627777a70d4a2a456e9e6d373fda8e7710e
Vitor Emanuel <veslima3@gmail.com>
2020-02-26 19:34:41 -0300

    Create exercicio63.c

---
+++ exercicio63.c
@@ -0,0 +1,34 @@
+/*
+
+Escreva um programa que leia dois vetores de números reais de mesma dimensão (10
+posições), e imprima o vetor resultante da soma destes vetores.
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    int vetor1[10], vetor2[10], vet3[20], i;
+    
+    for(i = 0; i <= 9; i++){
+        
+        vetor1[i] = i;
+        //printf("\n%d", vetor1[i]);
+        
+    }
+    
+    for(i = 10; i <= 19; i++){
+        
+        vetor2[i] = i;
+        //printf("\n%d", vetor2[i]);
+        
+    }
+    
+    for(i = 0; i <= 19; i++){
+        
+        
+        
+    }
+
+    return 0;
+}
