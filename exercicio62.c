8bdce7fdca0dbbe9591ca8bc7271b460765a370f
Vitor Emanuel <veslima3@gmail.com>
2020-02-27 00:06:19 -0300

    Update exercicio62.c

--- exercicio62.c
+++ exercicio62.c
@@ -1,3 +1,4 @@
+  
 /*Faça um programa em C que leia um array de 10 posições e conte quantos números pares
 são elementos do array. Imprima esta quantidade.*/
 
@@ -10,16 +11,16 @@ int main()
     for(i = 0; i <= 9; i++){
         
         array[i] = i;
+        printf("\narray[%d] = %d", array[i], i);
         
         if(i%2 == 0 && i != 0){
             
-        //printf("\narray[%d] = %d", i, array[i]);
         contador++; 
         
         }
     }
     
-    printf("Há %d elementos pares no array.", contador);
+    printf("\n\nHá %d elementos pares no array.", contador);
 
     return 0;
 }
