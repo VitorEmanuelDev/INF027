ab64c352cb011ef060d3dd75a3c1b3fd9cb58834
Vitor Emanuel <veslima3@gmail.com>
2020-03-08 15:29:05 -0300

    Create exercicio64.c

---
+++ exercicio64.c
@@ -0,0 +1,47 @@
+/*Faça um programa em C que leia um array de 20 inteiros e imprima o menor e o maior
+valor dentre os elementos do array.*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int i, array[20], menor, maior;
+    
+    //printf("Informe os valores de cada elemento do array.\n");
+    
+    for(i = 0; i < 20; i++){
+        
+        array[i] = i;
+        
+    }
+    
+    maior = array[0];
+    
+    for(i = 0; i < 20; i++){
+     
+     if(array[i] > maior){
+         
+         maior = array[i];
+         
+        }   
+        
+    }
+    
+    menor = array[i];
+    
+    for(i = 0; i < 20; i++){
+     
+     if(array[i] < menor){
+         
+         menor = array[i];
+         
+        }   
+        
+    }
+    
+    printf("O maior número do array é: %d\n", maior);
+    printf("O menor número do array é: %d\n", menor);
+
+    return 0;
+}
