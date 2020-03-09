ec85863b47bfa71bc9bed25090bf5a12abe0cb7f
Vitor Emanuel <veslima3@gmail.com>
2020-03-09 20:41:09 -0300

    Create exercicio70.c

---
+++ exercicio70.c
@@ -0,0 +1,21 @@
+/*Escreva um programa que carregue um array com tamanho variável. O tamanho máximo
+do array é de 100 posições (carga de array com sentinela).*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i, array[100] = {};
+    
+    for(i = 0; i < 100; i++){
+        
+        array[i] = i;
+        printf("%d ", array[i]);
+        
+    }
+    
+    
+
+    return 0;
+}
+
