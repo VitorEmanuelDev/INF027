78500e259769dbf459c9176675178c9bea7adb03
Vitor Emanuel <veslima3@gmail.com>
2020-03-09 21:34:52 -0300

    Create exercicio71.c

---
+++ exercicio71.c
@@ -0,0 +1,32 @@
+/*
+
+Escreva um programa que leia um vetor de 10 posições de inteiros e um inteiro.
+O programa deve informar a primeira posição onde este inteiro ocorre no vetor 
+ou -1 caso o valor não ocorra no vetor (Busca sequencial).
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    int numero, valor, i, array[10] = {89 , 45, 23, 24 ,82 , 10 , 88 , 66 , 99 , 0};
+    
+    printf("Informe o número a ser procurado:\n");
+    scanf("%d", &numero);
+    
+    valor = -1;
+    
+    for(i = 0; i < 10; i++){
+        
+        if(array[i] == numero){
+            
+            valor = i;  
+            
+        }
+        
+    }
+    
+    printf("%d", valor);
+
+    return 0;
+}
