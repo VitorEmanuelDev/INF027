5fc79852055863b2749f14ee17cbd15e23dfea92
Vitor Emanuel <veslima3@gmail.com>
2020-03-13 00:29:51 -0300

    Update exercicio73.c

--- exercicio73.c
+++ exercicio73.c
@@ -5,11 +5,12 @@ b. A mediana dos elementos no array (elemento central)
 c. A média
 */
 #include <stdio.h>
-#define TAM_ARRAY 20
+#define TAM_ARRAY 22
 
 int main()
 {
-    int i, j, moda, armazena, contador = 0, array[] = {18,19,112,12,12,15,16,9,8,9,19,12,1,15,18,20,20,11,12,18};
+    int i, j, moda, contaModa, atual, contaAtual, armazena,
+    array[] = {18,19,12,18,12,14,16,9,8,11,16,12,1,15,18,20,20,18,12,18,14,13};
     float media, soma = 0, mediana;
     
     //ordenar array
@@ -38,20 +39,33 @@ int main()
     }
     
     //determinar a moda do array
-    for (i =  0; i < TAM_ARRAY; i++){
-        
-        for(j = 0; j < TAM_ARRAY; j++){
-            
-            
-            
-        }
-        
-    }
+    contaModa = 0;
+    
+	for(i = 0; i < TAM_ARRAY; i++){
+	    
+	  atual = array[i]; 
+  	  contaAtual = 0;
+  	  
+	  while(atual == array[i]){
+	      
+	    i++;  
+	  	contaAtual++;
+	  	
+	  }	
+	  
+	  if(contaAtual > contaModa){
+	      
+	  	contaModa = contaAtual;
+	  	moda = atual;
+	  	
+	  }
+	  
+	}
 
     //calculo da mediana
     if(TAM_ARRAY%2 == 0){
         
-        mediana = array[TAM_ARRAY/2] + array[(TAM_ARRAY/2) - 1];
+        mediana = (array[(TAM_ARRAY/2) - 1] + array[(TAM_ARRAY/2) + 1])/2;
         
     } else{ 
         
@@ -60,12 +74,12 @@ int main()
     }
     
     
-    media = soma/20;
+    media = soma/TAM_ARRAY;
     printf("Média: %.2f\n", media);
     
     printf("Mediana: %.2f\n", mediana);
     
-    //printf("Moda: %d", moda);
+    printf("Moda: %d", moda);
     
     return 0;
 }
