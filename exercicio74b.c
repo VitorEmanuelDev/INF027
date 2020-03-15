4c84b2b0f86e9e02e3dadc7354eeb43991e30217
Vitor Emanuel <veslima3@gmail.com>
2020-03-15 20:54:36 -0300

    Create exercicio74b.c

---
+++ exercicio74b.c
@@ -0,0 +1,41 @@
+/*Escreva um programa em C que armazene um vetor de até 30 inteiros. O programa deve
+fornecer as seguintes operações:
+
+a. Inserir um elemento no final do vetor
+b. Inserir um elemento em uma dada posição
+c. Remover um elemento de uma posição indicada
+d. Remover todos elementos iguais a um valor indicado
+e. Gerar um novo array sem duplicidades a partir deste array*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM_ARRAY 30
+
+int main()
+{
+    int i, j, k, array[TAM_ARRAY] = {0,4,5,7,8,9,6,3,2,1,4,5,8,9,7,6,5,3,4,8,7,9,8,5,2,4,1,0,3,0};
+    int final, posInsert, posRemove, igual;
+    int contador = 0, armazena = 0, tamanho = 0, arrayNovo[] = {};
+    
+    //b. Inserir um elemento em uma dada posição
+    
+    printf("\nInforme um elemento e uma posição para inseri-lo.\n");
+    scanf("%d%d", &posInsert, &i);
+    
+    if(i < 0 || i > TAM_ARRAY){
+        
+        printf("Posição inválida.");
+        
+    }
+    
+    array[i] = posInsert;
+    
+    for(i = 0; i < TAM_ARRAY; i++){
+    
+    printf("%d ", array[i]);
+    
+    } 
+    
+    return 0;
+}
+
