0d723eb0a7a97a4310e840b17dd7a08c16df816b
Vitor Emanuel <veslima3@gmail.com>
2020-03-19 14:14:38 -0300

    Update exercicio76.c

--- exercicio76.c
+++ exercicio76.c
@@ -3,11 +3,30 @@ existem nesta string.*/
 
 #include <stdio.h>
 #include <string.h>
+#define TAM_ARRAY 100
 
-int main()
-{
-    printf("Informe uma palavra:\n");
-    fgets
+ int main()
+ {
+     char palavra[TAM_ARRAY];
+     int i, contador = 0;
 
-    return 0;
-}
+    puts("Informe uma palavra:\n"); 
+    fgets(palavra,TAM_ARRAY,stdin);
+
+    for(i = 0; palavra[i] != '\0'; i++){
+        
+        if(palavra[i] == 'a' || palavra[i] == 'e' || palavra[i] == 'i'
+           || palavra[i] == 'o' || palavra[i] == 'u'){
+            
+            contador++;
+           
+        }
+
+     }
+
+     printf("%s tem %d vogais.", palavra, contador);
+   
+     return 0;
+ }
+
+
