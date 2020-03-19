9d9e7e3bb11f670240d53f2b225aa199ed512a9d
Vitor Emanuel <veslima3@gmail.com>
2020-03-18 23:49:44 -0300

    Create exercicio76.c

---
+++ exercicio76.c
@@ -0,0 +1,13 @@
+/*Escreva um programa em C, que leia uma string e conte quantas ocorrências de vogais
+existem nesta string.*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    printf("Informe uma palavra:\n");
+    fgets
+
+    return 0;
+}
