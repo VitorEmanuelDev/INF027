a51abb6a60c24c40b98054d092b9263ed99cba4d
Vitor Emanuel <veslima3@gmail.com>
2020-04-01 21:40:53 -0300

    Update avaliacao02ano2013sem02Q2.c

--- avaliacao02ano2013sem02Q2.c
+++ avaliacao02ano2013sem02Q2.c
@@ -3,51 +3,48 @@ pares de números primos gêmeos são (3, 5), (5, 7), (11, 13), (17, 19), (29, 3
 Escreva um programa em C que imprima os primeiros N pares de números primos, onde N é uma entrada para o
 programa em questão.*/
 
-
 #include <stdio.h>
 
 int main()
 {
-    
-    int N, TAM_ARRAY, i, primos[TAM_ARRAY], index = 0, numero = 1;
-    
+
+    int N, i, primos[200], index = 0, numero = 1;
+
     printf("N = ");
     scanf("%d", &N);
-    
-    TAM_ARRAY = N;
-   
+
     while(numero <= N){
-        
+
         int divisor = 0;
-        
+
         for(i = 1; i <= numero; i++){
-            
+
             if(numero%i == 0)
                 divisor++;
-                
+
         }
-       
+
         if(divisor == 2){
-            
+
             primos[index] = numero;
             index++;
-            
+
         }
-       
+
         numero++;
     }
-   
-    for(i = 0; i <= TAM_ARRAY; i += 2){
-        
-        if (primos[i] != 0 && primos[i+1] != 0){
-            
+
+    for(i = 0; i <= 200; i += 2){
+
+        if (primos[i] != 0 && primos[i+1] != 0 && primos[i] > 0 && primos[i+1] > 0 &&
+            primos[i] <= N && primos[i+1] <= N){
+
             printf(" %d e %d \n", primos[i], primos[i+1]);
-            
+
         }
     }
-    
-    
-   return 0;
-   
-}
 
+
+   return 0;
+
+}
