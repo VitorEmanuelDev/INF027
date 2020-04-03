cece90d788f30892c96497c14214e66f9d1f184c
Vitor Emanuel <veslima3@gmail.com>
2020-04-03 00:27:49 -0300

    Update exercicio74e.c

--- exercicio74e.c
+++ exercicio74e.c
@@ -1,6 +1,5 @@
 /*Escreva um programa em C que armazene um vetor de até 30 inteiros. O programa deve
 fornecer as seguintes operações:
-
 a. Inserir um elemento no final do vetor
 b. Inserir um elemento em uma dada posição
 c. Remover um elemento de uma posição indicada
@@ -9,15 +8,20 @@ e. Gerar um novo array sem duplicidades a partir deste array*/
 
 #include <stdio.h>
 #include <stdlib.h>
-#define TAM_ARRAY 30
 
 int main()
 {
-    int i, j, k = 0, array[TAM_ARRAY] = {0,4,5,7,8,9,6,3,2,1,4,5,8,9,7,6,5,3,4,8,7,9,8,5,2,4,1,0,3,0};
+    int TAM_ARRAY = 30, i, j, k, array[TAM_ARRAY];
     int final, posInsert, posRemove, igual;
-    int contador = 0, armazena = 0, tamanho = 0, arrayNovo[] = {};
+    int contador = 0, armazena = 0, tamanho = 0;
 
     //e. Gerar um novo array sem duplicidades a partir deste array
+    
+    for(i = 0; i < TAM_ARRAY; i++){
+	    
+	    array[i] = rand() % 30;
+	    
+	}
     
     printf("\nArray sem duplicidades:\n");
     
@@ -25,40 +29,29 @@ int main()
         
         for(j = i + 1; j < TAM_ARRAY; j++){
             
-            if(array[i] > array[j]){
+            if(array[i] == array[j]){
                 
-                armazena = array[i];
-                array[i] = array[j];
-                array[j] = armazena;
-              
-               
+                for(k = j; k < TAM_ARRAY; k++){
+                    
+                    array[k] = array[k + 1];
+                }
+
+                TAM_ARRAY--;//diminuir o tamanho da lista depois de jogar o elemento repetido no array fantasm
+
+                j--;//não incrementar o j se houver troca. senão pode ocorrer de um elemento repetido ser pulado
             }
-            
         }
+    }
+
+
+    printf("\nArray sem duplicidade: ");
+    for(i = 0; i < TAM_ARRAY; i++){
+        
+        printf("%d ", array[i]);
         
     }
-    
-    for(j = 0; j < TAM_ARRAY - 1; j++){
-        
-        //printf("%d ", array[i]);
-        if(array[j] != array[j + 1]){
-            
-            arrayNovo[k] = array[j];
-            k++;
-            tamanho++;
-        }
-        
-    }
-    
-    arrayNovo[k] = arrayNovo[tamanho];
-    
-    for(k = 1; k < tamanho; k++){
-        
-        printf("%d ", arrayNovo[k]);
-        
-    }
+
     
     
     return 0;
 }
-
