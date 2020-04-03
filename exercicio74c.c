662edc1b25f405bd4138f46c920195ab3b1e03eb
Vitor Emanuel <veslima3@gmail.com>
2020-04-03 01:30:10 -0300

    Update exercicio74c.c

--- exercicio74c.c
+++ exercicio74c.c
@@ -10,18 +10,16 @@ e. Gerar um novo array sem duplicidades a partir deste array*/
 
 #include <stdio.h>
 #include <stdlib.h>
-#define TAM_ARRAY 30
 
 int main()
 {
-
-    int i, j, k, array[TAM_ARRAY] = {0,4,5,7,8,9,6,3,2,1,4,5,8,9,7,6,5,3,4,8,7,9,8,5,2,4,1,0,3,0};
+    int TAM_ARRAY = 30, i, j, k, array[TAM_ARRAY];
     int final, posInsert, posRemove, igual;
-    int contador = 0, armazena = 0, tamanho = 0, arrayNovo[] = {};
+    int continuar = 0;
 
-//c. Remover um elemento de uma posição indicada
+    //c. Remover um elemento de uma posição indicada
     
-    printf("\nInforme uma posição para deletar o elemento que ela contém.\n");
+    printf("\nInforme uma posição a ser deletada.\n");
     scanf("%d", &posRemove);
     
     
@@ -32,11 +30,11 @@ int main()
     }
     
     
-    for(i = 0; i < TAM_ARRAY - 1; i++){
+    for(i = 0; i <= TAM_ARRAY - 1; i++){
         
         printf("%d ", array[i]);    
         
     }
-        
+    
     return 0;
 }
