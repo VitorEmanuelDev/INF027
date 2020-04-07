c72303458fbe6426801a654152453063ec735cc1
Vitor Emanuel <veslima3@gmail.com>
2020-04-07 02:44:19 -0300

    Create avaliacao02_Q3_2016_2.c

---
+++ avaliacao02_Q3_2016_2.c
@@ -0,0 +1,13 @@
+/*Dizemos que um número i é congruente módulo m a j se i % m = j % m.
+Por exemplo, exemplo: 35 é congruente módulo 4 a 39, pois 35 % 4 = 3 = 39 % 4.
+Escreva um programa em C, que leia i, j e m e imprima todos os pares de números i*
+congruente modulo m a j*, onde i*<= i e j*<= j.*/
+
+#include <stdio.h>
+
+int main()
+{
+    
+
+    return 0;
+}
