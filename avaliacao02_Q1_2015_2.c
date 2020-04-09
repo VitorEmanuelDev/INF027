40914fe3900bcfb0999d2d20bd109ea9dc1fb9c5
Vitor Emanuel <veslima3@gmail.com>
2020-04-09 01:39:10 -0300

    Update avaliacao02_Q1_2015_2.c

--- avaliacao02_Q1_2015_2.c
+++ avaliacao02_Q1_2015_2.c
@@ -9,19 +9,36 @@ int main()
     int i, contagem = 1;
     float chico = 1.5, ze = 1.1;
     
-    for(i = 1; i <= 100; i++){
-            
-            chico += 0.02;
-            ze += 0.03;
-            //printf("%.2f ; %.2f ", chico, ze);
-            contagem++;
-            
-            if(ze > chico){
-                
-                printf("Zé será maior que Chico depois de %d anos.", contagem);
-                break;
-                
-            }
+    //for(i = 1; i <= 100; i++){
+    //        
+    //        chico += 0.02;
+    //        ze += 0.03;
+    //        //printf("%.2f ; %.2f ", chico, ze);
+    //        contagem++;
+    //        
+    //        if(ze > chico){
+    //            
+    //            printf("Zé será maior que Chico depois de %d anos.", contagem);
+    //            break;
+    //            
+    //        }
+    //    
+    //}
+    
+    
+    i = 1;
+    
+    while(ze <= chico){
+        
+        chico += 0.02;
+        ze += 0.03;
+        
+        i++;
+    }
+    
+    if(ze > chico){
+        
+        printf("Zé será maior que Chico depois de %d anos.", i);
         
     }
 
