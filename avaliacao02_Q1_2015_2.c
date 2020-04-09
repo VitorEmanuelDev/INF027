750e111d6c83f4fe003a2b581defef47ebfaa1cc
Vitor Emanuel <veslima3@gmail.com>
2020-04-09 01:29:25 -0300

    Create avaliacao02_Q1_2015_2.c

---
+++ avaliacao02_Q1_2015_2.c
@@ -0,0 +1,29 @@
+/*Chico tem 1,50 metro e cresce 2 centímetros por ano, enquanto Zé tem 1,10 metro e cresce
+3 centímetros por ano. Construa um programa em C que calcule e imprima quantos anos
+serão necessários para que Zé seja maior que Chico.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i, contagem = 1;
+    float chico = 1.5, ze = 1.1;
+    
+    for(i = 1; i <= 100; i++){
+            
+            chico += 0.02;
+            ze += 0.03;
+            //printf("%.2f ; %.2f ", chico, ze);
+            contagem++;
+            
+            if(ze > chico){
+                
+                printf("Zé será maior que Chico depois de %d anos.", contagem);
+                break;
+                
+            }
+        
+    }
+
+    return 0;
+}
