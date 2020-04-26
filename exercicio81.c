e90efe2a96715313dd4b5816955560abfdffcda7
Vitor Emanuel <veslima3@gmail.com>
2020-04-25 21:31:29 -0300

    Create exercicio81.c

---
+++ exercicio81.c
@@ -0,0 +1,31 @@
+/*Escreva um programa em C que normalize uma string lida, em uma nova string.
+Normalizar uma string é o processo de remover os espaços excedentes que separam as
+palavras.*/
+
+#include <stdio.h>
+#include <string.h>
+#define size 50
+
+int main()
+{
+	char str[size], strNorm[size], aux[size];
+	int i, j = 0;
+	
+	printf("Escreva uma frase ou palavra:\n");
+	fgets(str, size, stdin);
+	
+    for(i = 0; str[i] != '\0'; i++){
+        
+        //printf("%c", str[i]);
+        
+        if(str[i] != ' '){
+            
+            strNorm[j] = str[i];
+            j++;
+            
+        }
+        
+    }
+    
+    printf("%s", strNorm);
+}
