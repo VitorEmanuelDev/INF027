0fe35c6f0506d6ac7c08d471c4cc4b92d53b7ba2
Vitor Emanuel <veslima3@gmail.com>
2020-04-27 00:13:15 -0300

    Update exercicio52.c

--- exercicio52.c
+++ exercicio52.c
@@ -8,30 +8,48 @@ anteriores, ou seja:
 Criar um algoritmo em C que imprima os N primeiros termos da série de
 FETUCCINE, sabendo-se que para existir esta série serão necessários pelo menos três termos.*/
 
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main(){
-int termo1, termo2, i, termo3;
-
-    printf("Informe o primeiro termo:\n ");
-    scanf("%d", &termo1);
+    int termo1, termo2, limite, i, termo;
     
-    printf("Informe o segundo termo:\n ");
-    scanf("%d", &termo2);
-
-    for(i = 3; i <= 10; i++){
-
-        if(i%2 == 0 || i%2 == 0){
+    printf("Informe o primeiro termo:\n");
+    scanf("%d", &termo1);// 1
     
-            termo3 = (i - 1) - (i - 2);
-            printf("%d ", termo3);
-
-        } else {
-            
-            termo3 = (i - 1) + (i - 2);
-            printf("%d ", termo3);
-            
-                
-        }
+    printf("Informe o segundo termo:\n");
+    scanf("%d", &termo2);//2
+    
+    printf("Digite a quantidade de termos:\n");
+    scanf("%d", &limite);//10
+    
+    if(limite < 3){
+    
+        printf("A quantidade mínima de termos é 3.\n");
+    
     }
-}
+    
+    printf("%d %d ", termo1, termo2);
+  
+    for(i = 0; i < limite; i++){
+        
+        if(i % 2 != 0){
+        
+            termo = termo2 - termo1; 
+            
+        }else if (i % 2 == 0){
+            
+            termo = termo2 + termo1;
+            
+        }
+        
+        printf("%d ", termo);//1 2 3 1 4 3 7 9 11 7
+        
+        termo1 = termo2;
+        termo2 = termo;
+    }
+    
+    return 0;
+    
+}   
