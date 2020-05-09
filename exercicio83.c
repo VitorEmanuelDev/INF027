1ebdbcee371459d0ea26a13c9c5a5f2a04bb92dd
Vitor Emanuel <veslima3@gmail.com>
2020-05-09 00:13:48 -0300

    Create exercicio83.c

---
+++ exercicio83.c
@@ -0,0 +1,35 @@
+/*Escreva um programa em C, que gere a substring de uma string original, dada a posição
+inicial e a final da substring.*/
+
+#include <stdio.h>
+#include <string.h>
+#define TAM_ARRAY 200
+
+int main(){
+    
+   char frase[TAM_ARRAY], palavra[TAM_ARRAY];
+   int i, inicio, fim;
+ 
+   printf("Informe uma frase:\n");
+   fgets(frase, TAM_ARRAY, stdin);
+ 
+   printf("Informe a posição da palavra que deseja extrair:\n");
+   scanf("%d", &inicio);
+     
+   printf("Informe quantas letras a palavra que deseja extrair tem:\n");
+   scanf("%d",&fim);
+   
+ 
+    for(i = 0; i < fim; i++) {
+      
+      palavra[i] = frase[inicio + i];
+     
+    }
+   
+   palavra[i] = '\0'; // sem colocar essa parte haverá lixo de memória
+   
+   printf("%s", palavra);
+   
+   return 0;
+   
+}
