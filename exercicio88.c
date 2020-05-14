1fe18e95b7143849c5540d4d63bd81b53dd358eb
Vitor Emanuel <veslima3@gmail.com>
2020-05-14 00:36:26 -0300

    Create exercicio88.c

---
+++ exercicio88.c
@@ -0,0 +1,28 @@
+/*Escreva um programa em C, que coloque a string lida toda em caixa alta.*/
+
+
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#define size 30
+
+
+int main() 
+{ 
+    
+    int i = 0;
+
+    char str[size], stra; 
+    
+    printf("Escreva uma frase.\n");
+    fgets(str, size, stdin);
+    
+    for(i = 0; str[i] != '\0'; i++) {
+        
+        stra = str[i]; 
+        printf("%c", toupper(stra)); 
+     
+    } 
+    
+    return 0; 
+} 
