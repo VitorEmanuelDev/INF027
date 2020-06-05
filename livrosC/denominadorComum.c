e699f9af5fdad95723d07d6337df7a66c1fe4d92
Vitor Emanuel <veslima3@gmail.com>
2020-06-05 02:31:52 -0300

    Create denominadorComum.c

---
+++ livrosC/denominadorComum.c
@@ -0,0 +1,37 @@
+/* Denominador comum */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int denominador(int num1, int num2);
+
+int main()
+{
+    
+    int resultado = 0, num1, num2;
+    
+    printf("Informe dois números inteiros para encontrarmos o denominador comum:\n");
+    scanf("%d%d", &num1, &num2);
+    
+    resultado = denominador(num1, num2);
+    
+    printf("O denominador comum de 150 e de 35 é %d.",  resultado);
+    
+    return 0;
+}
+
+int denominador(int num1, int num2){
+    
+    int aux;
+    
+    while(num2 != 0){
+        
+        aux = num1 % num2;
+        num1 = num2;
+        num2 = aux;
+        
+    }
+    
+    return num1;
+    
+}
