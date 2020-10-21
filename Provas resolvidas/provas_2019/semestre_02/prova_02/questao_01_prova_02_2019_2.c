93d421ed15ac0ac0633dec1381c1b79dd12a723d
Vitor Emanuel <veslima3@gmail.com>
2020-10-21 00:19:59 -0300

    Update questao_01_prova_02_2019_2.c

--- Provas resolvidas/provas_2019/semestre_02/prova_02/questao_01_prova_02_2019_2.c
+++ Provas resolvidas/provas_2019/semestre_02/prova_02/questao_01_prova_02_2019_2.c
@@ -18,7 +18,7 @@ existem 5 subsequencias cuja soma dos números é igual a K = 5.
 
 #include <stdio.h>
 #include <stdlib.h>
-//#define TAM 10//teste
+#define TAM 500//teste
 
 int main(void) {
 
@@ -44,7 +44,7 @@ int main(void) {
 
 	}
 
-	int sequencia[tamanho];// = {1,0,2,2,0,0,8,5,3,2};//teste
+	int sequencia[TAM];// = {1,0,2,2,0,0,8,5,3,2};//teste
 
 	printf("Informe os valores da sua sequencia:");
 
@@ -57,64 +57,19 @@ int main(void) {
 
 	}
 
-	int soma = 0;
-	int contador = 0;
 	int subsequencia = 0;
 
 	for(int iteracao = 0; iteracao < tamanho; iteracao++){
 
-		//reiniciar o loop com o contador para que a leitura do array recomece a partir do ponto de referencia atual e não do início 0
-		for(int subcontagem = contador; subcontagem < tamanho; subcontagem++){
+		int soma = 0;
+
+		for(int subcontagem = iteracao; subcontagem < tamanho; subcontagem++){
 
 			soma += sequencia[subcontagem];
-			contador++;
-			//printf("%d ", soma);//teste
 
-			if(soma == valorMax && sequencia[subcontagem + 1] == 0){
-							  //e
+			if(soma == valorMax){
+
 				subsequencia++;
-				break;
-
-			}else if(soma == valorMax && sequencia[subcontagem + 1] != 0){
-									//e
-				soma = 0;
-				subsequencia++;
-				break;
-
-			}else if(soma > valorMax && sequencia[subcontagem] == valorMax && sequencia[subcontagem + 1] == 0){
-								  //mas								      //e
-				soma = sequencia[subcontagem];
-				subsequencia++;
-				break;
-
-			}else if(soma > valorMax && sequencia[subcontagem] == valorMax && sequencia[subcontagem + 1] != 0){
-								   //mas								  //e
-				soma = 0;
-				subsequencia++;
-				break;
-
-			} else if(soma > valorMax){
-
-				soma = 0;
-				break;
-
-			} else if(sequencia[subcontagem] + sequencia[subcontagem + 1] == valorMax){
-			    
-			    subsequencia++;
-				break;
-			    
-			}
-
-        		if(sequencia[subcontagem] == valorMax && sequencia[subcontagem + 1] == 0){
-												//e
-				subsequencia++;
-				break;
-
-			}else if(sequencia[subcontagem] == valorMax && sequencia[subcontagem + 1] != 0){
-													//mas
-				soma = 0;
-				subsequencia++;
-				break;
 
 			}
 
@@ -133,6 +88,6 @@ cenários de teste:
 [7 7 7 7 5 2 1 5 9 0]
 [1 2 3 5 6 7 2 1 6 1] 3
 [1,2,0,3,5] 2
+[1,2,2,0,5,3,2,0,0,0] 5
 */
-
 
