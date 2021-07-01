7a8ed7cc112d75a46db7879956d168d8c231630b
Vitor Emanuel <veslima3@gmail.com>
2021-07-01 18:17:00 -0300

    Create exercicio100.c

---
+++ Lista Logica de Programacao/exercicio100.c
@@ -0,0 +1,3 @@
+/*100. Verificar se uma matriz dada forma um Quadrado Latino de ordem N, no qual
+em cada linha e em cada coluna aparecem todos os inteiros 1,2,3, ... N, ou seja, cada linha
+ou coluna é permutação dos N primeiros números inteiros. */
